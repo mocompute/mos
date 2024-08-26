2496c7acb66d2d7dd0b6f7677a7785142adf52ba
mo <mocompute@outlook.com>
2024-08-26 21:37:29 +1200

    streql

--- src/root.zig
+++ src/root.zig
@@ -1,13 +1,11 @@
-//! By convention, root.zig is the root source file when making a library. If
-//! you are making an executable, the convention is to delete this file and
-//! start with main.zig instead.
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+pub fn streql(a: []const u8, b: []const u8) bool {
+    return std.mem.eql(u8, a, b);
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+test streql {
+    try testing.expect(streql("hello", "hello"));
+    try testing.expect(!streql("hello", "world"));
 }
