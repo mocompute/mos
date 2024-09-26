ef2515e41b975e6c5977acb6ce077c20df19fadc
mo <mocompute@outlook.com>
2024-09-26 21:02:53 +1200

    make test reference all declarations

--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,10 @@
+const std = @import("std");
+
 pub const streql = string.streql;
 
 pub const string = @import("string.zig");
 pub const file = @import("file.zig");
+
+test {
+    std.testing.refAllDecls(@This());
+}
