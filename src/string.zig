08c263a06396ff60411de42e796836dbf32d02ec
mo <mocompute@outlook.com>
2024-09-28 13:43:19 +1200

    remove streql, change license, bump version to v0.1.0

--- src/string.zig
+++ src/string.zig
@@ -1,11 +1,18 @@
+// This file is part of mos.
+//
+// Copyright (C) 2024 <https://codeberg.org/mocompute>
+//
+// r-repo-parse is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// r-repo-parse is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 const std = @import("std");
 const testing = std.testing;
-
-pub fn streql(a: []const u8, b: []const u8) bool {
-    return std.mem.eql(u8, a, b);
-}
-
-test streql {
-    try testing.expect(streql("hello", "hello"));
-    try testing.expect(!streql("hello", "world"));
-}
