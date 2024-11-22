048b71c543383103396efa59ca018d6a79af8f22
mo <mocompute@outlook.com>
2024-11-22 16:11:07 +1300

    fix copy-and-pasted file headers

--- src/string.zig
+++ src/string.zig
@@ -2,12 +2,12 @@
 //
 // Copyright (C) 2024 <https://codeberg.org/mocompute>
 //
-// r-repo-parse is free software: you can redistribute it and/or modify
+// mos is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// r-repo-parse is distributed in the hope that it will be useful,
+// mos is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
